38c77b9cac8c0adf0451cfe3a6a12ca038740706
Matias Berrutti <berrutti@gmail.com>
2019-07-20 19:44:01 -0300

    First layout

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const createTestCafe = require('testcafe');
 const { app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
 
 let runner = null;
 let mainWindow = null;
 
 function createWindow() {
+    let state = windowStateKeeper({
+        defaultWidth: 1000,
+        defaultHeight: 800
+    });
+
     mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 800,
+        x: state.x,
+        y: state.y,
+        width: state.width,
+        height: state.height,
+        minWidth: state.width,
+        minHeight: state.height,
         webPreferences: { nodeIntegration: true}
     })
 
-    mainWindow.loadFile('./renderer/index.html');
+    mainWindow.loadFile('./renderer/main.html');
 
-    mainWindow.webContents.openDevTools();
+    state.manage(mainWindow);
+    // mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', ()=>{
         mainWindow = null;
@@ -27,16 +38,16 @@ app.on('window-all-closed', ()=>{
         app.quit();
 })
 
-createTestCafe('localhost', 1337, 1338)
-    .then(testcafe => {
+// createTestCafe('localhost', 1337, 1338)
+//     .then(testcafe => {
         
-        runner = testcafe.createRunner();
+//         runner = testcafe.createRunner();
 
-        return runner
-            .src(['tests/fixture1.js'])
-            .browsers(['chrome'])
-            .run()
-            .then(failedCount => {
-                console.log(failedCount);
-            })
-        });
+//         return runner
+//             .src(['tests/fixture1.js'])
+//             .browsers(['chrome'])
+//             .run()
+//             .then(failedCount => {
+//                 console.log(failedCount);
+//             })
+//         });
